baf85b8122745733909fca0ce17531c4f911b177
Dracula <chao.tan333@139.com>
2026-01-25 17:42:37 +0800

    优化 21.合并两个有序链表算法，添加递归实现并注释迭代实现

--- linked-list/easy/21_merge-two-sorted-lists.py
+++ linked-list/easy/21_merge-two-sorted-lists.py
@@ -61,6 +61,33 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
+        # 1.迭代法
+        # dummy = ListNode(-1)
+        # cur = dummy
+
+        # while list1 and list2:
+        #     if list1.val <= list2.val:
+        #         cur.next = list1
+        #         list1 = list1.next
+        #     else:
+        #         cur.next = list2
+        #         list2 = list2.next
+        #     cur = cur.next
+        # cur.next = list1 if list1 else list2
+
+        # return dummy.next
+
+        # 2.递归法
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
 # @lc code=end
 
--- linked-list/easy/21_合并两个有序链表.py
+++ linked-list/easy/21_合并两个有序链表.py
@@ -58,25 +58,32 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # 1.迭代法
+        # dummy = ListNode(-1)
+        # cur = dummy
+
+        # while list1 and list2:
+        #     if list1.val <= list2.val:
+        #         cur.next = list1
+        #         list1 = list1.next
+        #     else:
+        #         cur.next = list2
+        #         list2 = list2.next
+        #     cur = cur.next
+        # cur.next = list1 if list1 else list2
+
+        # return dummy.next
+
+        # 2.递归法
         if not list1:
             return list2
         if not list2:
             return list1
-        
-        head = ListNode(0)
-        cur = head
-        while list1 and list2:
-            if list1.val <= list2.val:
-                cur.next = list1
-                list1 = list1.next
-            else:
-                cur.next = list2
-                list2 = list2.next
-            cur = cur.next
-        if not list1:
-            cur.next = list2
-        else:
-            cur.next = list1
 
-        return head.next   
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2 
 # @lc code=end
