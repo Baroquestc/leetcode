e1107f357f2b24675cd2b53f02988293a245ee8d
Dracula <chao.tan333@139.com>
2026-01-28 10:30:38 +0800

    添加283.移动零算法实现，包含双指针法的两种实现方式

--- two-pointers/easy/283_移动零.py
+++ two-pointers/easy/283_移动零.py
@@ -1,46 +1,7 @@
-# @before-stub-for-debug-begin
-from python3problem283 import *
-from typing import *
-# @before-stub-for-debug-end
-
 #
 # @lc app=leetcode.cn id=283 lang=python3
 #
-# [283] Move Zeroes
-#
-# https://leetcode.cn/problems/move-zeroes/description/
-#
-# algorithms
-# Easy (64.03%)
-# Likes:    2694
-# Dislikes: 0
-# Total Accepted:    1.9M
-# Total Submissions: 3M
-# Testcase Example:  '[0,1,0,3,12]'
-#
-# Given an integer array nums, move all 0's to the end of it while maintaining
-# the relative order of the non-zero elements.
-# 
-# Note that you must do this in-place without making a copy of the array.
-# 
-# 
-# Example 1:
-# Input: nums = [0,1,0,3,12]
-# Output: [1,3,12,0,0]
-# Example 2:
-# Input: nums = [0]
-# Output: [0]
-# 
-# 
-# Constraints:
-# 
-# 
-# 1 <= nums.length <= 10^4
-# -2^31 <= nums[i] <= 2^31 - 1
-# 
-# 
-# 
-# Follow up: Could you minimize the total number of operations done?
+# [283] 移动零
 #
 
 # @lc code=start
